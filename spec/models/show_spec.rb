d405dd97ca78180d8e97075098b2fda5555b9777
Eugene <q010@bk.ru>
2019-01-14 00:26:16 +0300

    specs for Show

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Show, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(title: "Anything", description: "Anything")
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
 end
