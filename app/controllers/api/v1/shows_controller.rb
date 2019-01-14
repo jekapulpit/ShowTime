4761742329af428e9bc3a5e112cb9b4d7f5bb8d9
Eugene <q010@bk.ru>
2019-01-14 16:52:59 +0300

    adding show action to ShowsController

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -4,4 +4,30 @@ class Api::V1::ShowsController < ApplicationController
   def index
     render json: Show.all
   end
+
+  def show
+    show = Show.find(params[:id])
+    render json: show
+  end
+
+  def create
+    show = Show.create(show_params)
+    render json: show
+  end
+
+  def destroy
+    Show.destroy(params[:id])
+  end
+
+  def update
+    show = Show.find(params[:id])
+    show.update_attributes(show_params)
+    render json: show
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:name, :description)
+  end
 end
