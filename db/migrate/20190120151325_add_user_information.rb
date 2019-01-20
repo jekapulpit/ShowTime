27036a99cfffa7f1846dc7e5e22f57ea2d0e2720
Eugene <q010@bk.ru>
2019-01-20 18:21:28 +0300

    adding user information

---
+++ db/migrate/20190120151325_add_user_information.rb
@@ -0,0 +1,7 @@
+class AddUserInformation < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :realname, :string
+    add_column :users, :dateofbirth, :date
+    add_column :users, :moreinfo, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_13_212055) do
+ActiveRecord::Schema.define(version: 2019_01_20_151325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +57,9 @@ ActiveRecord::Schema.define(version: 2019_01_13_212055) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "realname"
+    t.date "dateofbirth"
+    t.text "moreinfo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
