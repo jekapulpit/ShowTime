48880b58d3ec58d7db4083d3c511cf2b098ac8fc
Eugene <q010@bk.ru>
2019-01-21 03:13:45 +0300

    update method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@
 
 class UsersController < ApplicationController
   def profile; end
+
+  def update
+    current_user.update_attributes(user_update_params)
+    render json: current_user
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :email, :avatar, :dateofbirth, :realname, :moreinfo)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:dateofbirth, :realname, :moreinfo)
+  end
 end
